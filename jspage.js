94d9c967f7f4006f6ead90be2a6954d83ac9ad69
genjiguru <54900323+jasmineguru@users.noreply.github.com>
2019-10-15 10:27:31 -0400

    dd

--- jspage.js
+++ jspage.js
@@ -9,16 +9,13 @@ function multiples(){
     if(user_num %4 && user_num %10==0){
         alert("FourTen");
     }
-  else(user_num %4 || user_num %10 != 0);
-}
-
+  else(user_num %4 || user_num %10 != 0)
+    }
+    
 
 function triangle(){
-
-    let a1= parseInt (prompt("Enter Angle 1"));
-    let a12= parseInt (prompt("Enter Angle 2"));
-    let a3= 180-(a2 + a1);
-    alert ("Angle 3 is " + a3);
+    let A1= parseInt ("Enter Angle 1")
+    let A2= parseInt ("Enter Angle 2")
+    let A3= 180-(A2 + A3)
+    alert ("Angle 3 is " +A3)
 }
-
-
