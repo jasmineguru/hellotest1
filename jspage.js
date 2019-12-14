7eb249c74c4fe5154aecd7299daf0af4411badf3
genjiguru <54900323+jasmineguru@users.noreply.github.com>
2019-12-13 22:14:21 -0500

    fff

--- jspage.js
+++ jspage.js
@@ -57,25 +57,25 @@ function century(){
     alert (text)
   }
 
-function simpleEncryption(){
+  function simpleEncryption(){
 
-var alpha = 
-{
-  'a':'b','b':'c', 'c':'d', 'd':'e', 'e':'f', 'f':'g','g':'h', 'h':'i', 'i':'j', 'j':'k', 'k':'l', 'l':'m', 'm':'n', 'n':'o','o':'p', 'p':'q', 'q':'r', 'r':'s', 's':'t', 't':'u','u':'v','v':'w','w':'x','x':'y','y':'z','z':'a'
-}
-var a = prompt("Tell me your word");
-var text = a.replace(/[abcdefghijklmnopqrstuvwxyz]/g,m=>alpha[m]);
-alert("This is your encrypted text : " + text + " . Copy this to your clipboard");
+    var alpha = 
+      {
+        'a':'b','b':'c', 'c':'d', 'd':'e', 'e':'f', 'f':'g','g':'h', 'h':'i', 'i':'j', 'j':'k', 'k':'l', 'l':'m', 'm':'n', 'n':'o','o':'p', 'p':'q', 'q':'r', 'r':'s', 's':'t', 't':'u','u':'v','v':'w','w':'x','x':'y','y':'z','z':'a'
+      }
+    var a = prompt("Tell me your word");
+    var text = a.replace(/[abcdefghijklmnopqrstuvwxyz]/g,m=>alpha[m]);
+    alert("This is your encrypted text : " + text + " . Copy this to your clipboard");
   }
 
 
-function simpleDecryption(){
+  function simpleDecryption(){
 
- var alpha = 
- {
-  'b':'a', 'c':'b', 'd':'c', 'e':'d', 'f':'e', 'g':'f', 'h':'g', 'i':'h', 'j':'i', 'k':'j', 'l':'k', 'm':'l', 'n':'m', 'o':'n', 'p':'o', 'q':'p', 'r':'q', 's':'r', 't':'s', 'u':'t', 'v':'u', 'w':'v', 'x':'w', 'y':'x', 'z':'y', 'a':'z'
- }
- var b = prompt("Paste your encrypted message");
- var text = b.replace(/[abcdefghijklmnopqrstuvwxyz]/g,m=>alpha[m]);
- alert("This is your decrypted text : " + text + " !");
+    var alpha = 
+      { 
+        'b':'a', 'c':'b', 'd':'c', 'e':'d', 'f':'e', 'g':'f', 'h':'g', 'i':'h', 'j':'i', 'k':'j', 'l':'k', 'm':'l', 'n':'m', 'o':'n', 'p':'o', 'q':'p', 'r':'q', 's':'r', 't':'s', 'u':'t', 'v':'u', 'w':'v', 'x':'w', 'y':'x', 'z':'y', 'a':'z'
+      }
+    var b = prompt("Paste your encrypted message");
+    var text = b.replace(/[abcdefghijklmnopqrstuvwxyz]/g,m=>alpha[m]);
+    alert("This is your decrypted text : " + text + " !");
 }
