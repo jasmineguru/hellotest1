55e6a0c41e334e58af05c7efc85807a826402ed4
genjiguru <54900323+jasmineguru@users.noreply.github.com>
2019-12-13 19:09:59 -0500

    jip

--- jspage.js
+++ jspage.js
@@ -12,11 +12,12 @@ function multiples(){
   else(user_num %4 || user_num %10 != 0)
     }
     
+    
 
 function triangle(){
     let A1= parseInt ( prompt ("Enter Angle 1"));
     let A2= parseInt ( prompt ("Enter Angle 2"));
-    let A3= 180-(A2 + A3);
+    let A3= 180-(A2 + A1);
     alert ("Angle 3 is " +A3);
 }
 
@@ -26,3 +27,29 @@ function century(){
    alert("The century is" + century1);
    
    }
+
+   function password_check(userName, password){
+  
+    if (userName.toLowercase=="Hello"&& password=="12345"){
+      return(userName)
+    }
+    
+    else{
+      return("Log in declined")
+    }
+  }
+  console.log(password_check("Hello", "12345"));
+
+  function encryption(){
+
+    var alpha = {'a':'ranen','b':'amat', 'c':'mahisha', 'd':'mehdi', 'e':'aidan', 'f':'hesham','g':'kirimie', 'h':'omar', 'i':'roxanne', 'j':'mustafa', 'k':'zaid', 'l':'danish', 'm':'anish', 'n':'alex','o':'rizwan', 'p':'shaarif', 'q':'toshan', 'r':'pais', 's':'yunus', 't':'whitney','u':'mehkeh','v':'sriya','w':'nikola','x':'shams','y':'sathya','z':'korain' }
+    var a = prompt("Tell me your word");
+    text = a.replace(/[abcdefghijklmnopqrstuvwxyz]/g,m=>alpha[m]);
+     }
+  
+  function decryption(){
+
+    var alpha = {'ranen':'a', 'amat':'b', 'mahisha':'c','mehdi':'d', 'aidan': 'e','hesham':'f','kirimie':'g', 'omar':'h', 'roxanne': 'i', 'mustafa':'j','zaid':'k', 'danish': 'l', 'anish':'m', 'n':'alex', 'rizwan':'o','shaarif':'p', 'toshan':'q', 'pais':'r', 'yunus':'s', 'whitney':'t', 'mehkeh':'u','sriya':'v', 'nikola':'w', 'shams':'x', 'sathya': 'y', 'korain':'z'}
+    var b = prompt("What do you wish to decrypt(copy and past your encrypted word)");
+    text = b.replace(/[abcdefghijklmnopqrstuvwxyz]/g,m=>alpha[m]);
+  }
