8a2ef548e5887466e36b151a55b8a5c7e0c073cc
genjiguru <54900323+jasmineguru@users.noreply.github.com>
2019-12-13 21:17:51 -0500

    thankf

--- jspage.js
+++ jspage.js
@@ -41,60 +41,18 @@ function century(){
   console.log(password_check("Hello", "12345"));
 
   function encryption(){
-
-    var alpha = 
-    {
-      'a':'ranen','b':'amat', 'c':'mahisha', 'd':'mehdi', 'e':'aidan', 'f':'hesham','g':'kirimie', 'h':'omar', 'i':'roxanne', 'j':'mustafa', 'k':'zaid', 'l':'danish', 'm':'anish', 'n':'alex','o':'rizwan', 'p':'shaarif', 'q':'toshan', 'r':'pais', 's':'yunus', 't':'whitney','u':'mehkeh','v':'sriya','w':'nikola','x':'shams','y':'sathya','z':'korain' 
-      }
-    var alpha = 
-    {
-      // 'a':'b','b':'c', 'c':'d', 'd':'e', 'e':'f', 'f':'g','g':'h', 'h':'i', 'i':'j', 'j':'k', 'k':'l', 'l':'m', 'm':'n', 'n':'o','o':'p', 'p':'q', 'q':'r', 'r':'s', 's':'t', 't':'u','u':'v','v':'w','w':'x','x':'y','y':'z','z':'a' 
-      }
+ 
+    var alpha = {'a':'ranen','b':'amat', 'c':'mahisha', 'd':'mehdi', 'e':'aidan', 'f':'hesham','g':'kirimie', 'h':'omar', 'i':'roxanne', 'j':'mustafa', 'k':'zaid', 'l':'danish', 'm':'anish', 'n':'alex','o':'rizwan', 'p':'shaarif', 'q':'toshan', 'r':'pais', 's':'yunus', 't':'whitney','u':'mehkeh','v':'sriya','w':'nikola','x':'shams','y':'sathya','z':'korain' }
     var a = prompt("Tell me your word");
-<<<<<<< HEAD
-<<<<<<< HEAD
-    var text = a.replace(/[ranenamatmahishamehdiaidanheshamkirimieomarroxannemustafazaiddanishanishalexrizwanshaariftoshanpaisyunuswhitneymehkehsriyanikolashamssathyakorain]/g,m=>alpha[m]);
-    // abcdefghijklmnopqrstuvwxyz
-    alert (text + ' ' + 'copy the given output to clipboard');
-  }
-=======
     var text = a.replace(/[abcdefghijklmnopqrstuvwxyz]/g,m=>alpha[m]);
-    alert (text + ' ' + 'copy the given output to clipboard')
-     }
->>>>>>> parent of 18d92af... fre
-  
-  function decryption(){
-    var alpha = 
-    {
-      'ranen':'a', 'amat':'b', 'mahisha':'c','mehdi':'d', 'aidan': 'e','hesham':'f','kirimie':'g', 'omar':'h', 'roxanne': 'i', 'mustafa':'j','zaid':'k', 'danish': 'l', 'anish':'m', 'alex':'n', 'rizwan':'o','shaarif':'p', 'toshan':'q', 'pais':'r', 'yunus':'s', 'whitney':'t', 'mehkeh':'u','sriya':'v', 'nikola':'w', 'shams':'x', 'sathya': 'y', 'korain':'z'
-      // 'b':'a', 'c' :'b', 'd':'c', 'e':'d', 'f':'e', 'g':'f',  'h':'g', 'i':'h', 'j':'i', 'k':'j', 'l':'k', 'm':'l', 'n':'m', 'o':'n', 'p':'p', 'q':'p', 'r':'q', 's':'r', 't':'s', 'u':'t', 'v':'u', 'w':'v', 'x':'w', 'y':'x', 'z':'y', 'a': 'z'
-      }
-    var b = prompt("Insert the message you desire to decrypt by (copy and paste your encrypted output");
-<<<<<<< HEAD
-<<<<<<< HEAD
-    var text = b.replace(/[ranenamatmahishamehdiaidanheshamkirimieomarroxannemustafazaiddanishanishalexrizwanshaariftoshanpaisyunuswhitneymehkehsriyanikolashamssathyakorain]/g,m=>alpha[m])
-    // var text = b.replace(/[abcdefghijklmnopqrstuvwxyz]/g,m=>alpha[m]);
-=======
-    var text = b.replace(/[ranenamatmahishamehdiaidanheshamkirimieomarroxannemustafazaiddanishanishshivanirizwanshaariftoshanpaisyunuswhitneymehkehsriyanikolashamssathyakorain]/g,m=>alpha[m])
-    // var text = b.replace(/[azyxwvutsrqponmlkjihgfedcb]/g,m=>alpha[m]);
->>>>>>> parent of 08746a7... frer
-=======
-    // var text = b.replace(/[ranenamatmahishamehdiaidanheshamkirimieomarroxannemustafazaiddanishanishshivanirizwanshaariftoshanpaisyunuswhitneymehkehsriyanikolashamssathyakorain]/g,m=>alpha[m])
-    var text = b.replace(/[azyxwvutsrqponmlkjihgfedcb]/g,m=>alpha[m]);
-<<<<<<< HEAD
->>>>>>> parent of 99d07fa... rrr
-    alert ('The decrypted text is' + ' ' + text + '!');
-=======
-    text = a.replace(/[abcdefghijklmnopqrstuvwxyz]/g,m=>alpha[m]);
+    alert (text + ' ' +'copy this to clipboard')
      }
   
+ 
   function decryption(){
-
-    var alpha = {'ranen':'a', 'amat':'b', 'mahisha':'c','mehdi':'d', 'aidan': 'e','hesham':'f','kirimie':'g', 'omar':'h', 'roxanne': 'i', 'mustafa':'j','zaid':'k', 'danish': 'l', 'anish':'m', 'n':'alex', 'rizwan':'o','shaarif':'p', 'toshan':'q', 'pais':'r', 'yunus':'s', 'whitney':'t', 'mehkeh':'u','sriya':'v', 'nikola':'w', 'shams':'x', 'sathya': 'y', 'korain':'z'}
-    var b = prompt("What do you wish to decrypt(copy and past your encrypted word)");
-    text = b.replace(/[abcdefghijklmnopqrstuvwxyz]/g,m=>alpha[m]);
->>>>>>> parent of 15dc538... return text
-=======
-    alert ('The decrypted text is' + ' ' + text + '!')
->>>>>>> parent of 18d92af... fre
+ 
+    var alpha = {'ranen':'a', 'amat':'b', 'mahisha':'c','mehdi':'d', 'aidan': 'e','hesham':'f','kirimie':'g', 'omar':'h', 'roxanne': 'i', 'mustafa':'j','zaid':'k', 'danish': 'l', 'anish':'m', 'alex':'n', 'rizwan':'o','shaarif':'p', 'toshan':'q', 'pais':'r', 'yunus':'s', 'whitney':'t', 'mehkeh':'u','sriya':'v', 'nikola':'w', 'shams':'x', 'sathya': 'y', 'korain':'z'}
+    var b = prompt("What do you wish to decrypt(copy and paste your encrypted word)");
+    var text = b.replace(/[abcdefghijklmnopqrstuvwxyz]/g,m=>alpha[m]);
+    alert (text)
   }
