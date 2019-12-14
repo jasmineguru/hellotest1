d01b201d82cbc7b390437b00e84a6ea5fd6157fe
genjiguru <54900323+jasmineguru@users.noreply.github.com>
2019-12-13 22:11:43 -0500

    ffrerfe

--- jspage.js
+++ jspage.js
@@ -73,7 +73,7 @@ function simpleDecryption(){
 
  var alpha = 
  {
-  'b':'a', 'c' :'b', 'd':'c', 'e':'d', 'f':'e', 'g':'f',  'h':'g', 'i':'h', 'j':'i', 'k':'j', 'l':'k', 'm':'l', 'n':'m', 'o':'n', 'p':'p', 'q':'p', 'r':'q', 's':'r', 't':'s', 'u':'t', 'v':'u', 'w':'v', 'x':'w', 'y':'x', 'z':'y', 'a': 'z' 
+  'b':'a', 'c':'b', 'd':'c', 'e':'d', 'f':'e', 'g':'f', 'h':'g', 'i':'h', 'j':'i', 'k':'j', 'l':'k', 'm':'l', 'n':'m', 'o':'n', 'p':'o', 'q':'p', 'r':'q', 's':'r', 't':'s', 'u':'t', 'v':'u', 'w':'v', 'x':'w', 'y':'x', 'z':'y', 'a':'z'
  }
  var b = prompt("Paste your encrypted message");
  var text = b.replace(/[abcdefghijklmnopqrstuvwxyz]/g,m=>alpha[m]);
