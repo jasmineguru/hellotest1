a91d882a3844c8b2f9379a34aa63778656737c68
genjiguru <54900323+jasmineguru@users.noreply.github.com>
2020-08-19 12:36:40 -0400

    4uow

--- mygames.js
+++ mygames.js
@@ -1,3 +1,12 @@
-function jaken(){
-    
+const selectionButtons = document.querySelectorAll('[data-selection]')
+
+selectionButtons.forEach(selectionButtons => {
+     selectionButtons.addEventListener('click', e=> {
+          const selectionName = selectionButton.dataset.selection
+          makeSelection(selectionName)
+    })
+})
+
+function makeSelection(selection){
+    console.log(selection);
 }
