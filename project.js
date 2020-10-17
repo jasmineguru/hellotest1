4fca6ea0fd589263a4959ab32d394e398dcbce6d
genjiguru <54900323+jasmineguru@users.noreply.github.com>
2020-10-17 16:15:32 -0400

    oct17-2

--- project.js
+++ project.js
@@ -6,16 +6,16 @@ function guessthenumber(){
         guess = prompt("Guess the secret number (from 0 to 100) to unlock a message");
 
         if(guess == computernumber){
-            alert("Correct| You are a Homo Sapien")
+            alert("Correct| You are a Homo Sapien");
         }
         else if(guess < computernumber){
-            alert("Too low |  Where you going to the earth core?")
+            alert("Too low |  Where you going to the earth core?");
         }
         else if(guess > computernumber){
-            alert("Too high| What, are you trying to fly to the moon and play among the star?")
+            alert("Too high| What, are you trying to fly to the moon and play among the star?");
         }
         else{
-            alert("Undefined | Do you even know english, type in a number ")
+            alert("Undefined | Do you even know english, type in a number ");
         }
   
     }
